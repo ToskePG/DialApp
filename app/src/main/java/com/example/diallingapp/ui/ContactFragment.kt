af6129b4e95ac62a2cefaf13fbf18223c38e6ed5
Moderator <danilo.tosic.pmf@gmail.com>
2023-01-21 17:38:20 +0100

    Minor changes

--- app/src/main/java/com/example/diallingapp/ui/ContactFragment.kt
+++ app/src/main/java/com/example/diallingapp/ui/ContactFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -11,6 +12,8 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.diallingapp.R
 import com.example.diallingapp.databinding.FragmentContactBinding
 import com.example.diallingapp.ui.adapters.ContactAdapter
+import androidx.navigation.fragment.navArgs
+import com.example.diallingapp.models.Contact
 
 class ContactFragment : Fragment() {
 
@@ -18,6 +21,7 @@ class ContactFragment : Fragment() {
     private val binding get() = _binding!!
     private lateinit var adapter: ContactAdapter
     private val viewModel: ContactViewModel by viewModels()
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -30,8 +34,17 @@ class ContactFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initContactRecycler()
+        initListeners()
     }
+
+    private fun initListeners() {
+        binding.addContact.setOnClickListener{
+            cleanFragment()
+            initContactRecycler()
+            loadContacts()
+        }
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         clearBinding()
@@ -42,9 +55,21 @@ class ContactFragment : Fragment() {
 
     private fun initContactRecycler(){
         adapter = ContactAdapter(viewModel.contactList) {
-                contact -> findNavController().navigate(R.id.action_contactFragment_to_contactInfoFragment)
+                contact -> run {
+            findNavController().navigate(R.id.action_contactFragment_to_contactInfoFragment)
+        }
         }
         binding.contactRecycler.layoutManager = LinearLayoutManager(this.requireActivity(), LinearLayoutManager.VERTICAL, false)
         binding.contactRecycler.adapter = adapter
+    }
+
+    private fun loadContacts(){
+            binding.contactRecycler.isVisible = true
+    }
+
+    private fun cleanFragment(){
+        binding.emptyContactScreen.isVisible = false
+        binding.addContact.isVisible = false
+        binding.contanctInfo.isVisible = false
     }
 }
--- app/src/main/java/com/example/diallingapp/ui/ContactInfoFragment.kt
+++ app/src/main/java/com/example/diallingapp/ui/ContactInfoFragment.kt
@@ -7,6 +7,7 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.diallingapp.databinding.FragmentContactInfoBinding
@@ -33,6 +34,9 @@ class ContactInfoFragment : Fragment() {
     }
 
     private fun initListeners(){
+        binding.backButton.setOnClickListener{
+            findNavController().popBackStack()
+        }
         initCallRecycler()
     }
 
